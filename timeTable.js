19bf0cc182dde9b0e4ddd90642ed1e4b88565061
Subhransu Sekhar Satpathy <92420572+Subhransu-12@users.noreply.github.com>
2023-12-18 21:32:54 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,80 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '08-10 AM',
+        roomNumber: '37-604',
+        subject: 'CSE377',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '34-407',
+        subject: 'INT348',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '12-01 PM',
+        roomNumber: '34-407',
+        subject: 'INT348',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '08-10 AM',
+        roomNumber: '37-604',
+        subject: 'CSE377',
+        type: 'Practical'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-301',
+        subject: 'CSE378',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '34-407',
+        subject: 'INT348',
+        type: 'Practical'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '12-01 PM',
+        roomNumber: '34-407',
+        subject: 'INT348',
+        type: 'Practical'
+    }
+]
+
+const Friday =[
+    {   
+        time: 'Friday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
